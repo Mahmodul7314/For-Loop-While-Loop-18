35effc9d37b0aca9743324249d0cc79a20bd1c42
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-12-20 13:19:55 +0600

    for-loop-while-loo--m18

---
+++ loops.js
@@ -0,0 +1,13 @@
+///for loop
+
+// var i;
+// for (i=1; i<=20; i=i+1){
+//     console.log("Please Give Foods them"
+//     );
+// }
+var num;
+for(num=40; num<=80; num=num+1){
+    console.log(num);
+
+}
+
